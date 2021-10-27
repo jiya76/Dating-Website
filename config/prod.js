fd45bbcaa736d5e9ec3e2432b9460917978877fc
Jiya Chauhan <76962214+jiya76@users.noreply.github.com>
2021-10-28 04:47:32 +0530

    fb link updated

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,7 @@
 module.exports={
     MongoDB: process.env.MONGODB,
-    FacebookAppID: '993472138176883',
-    FacebookAppSecret: 'e4798f9c72fe78a13346acd8e7ca68db',
+    FacebookAppID: process.env.FB_APP,
+    FacebookAppSecret: 'PROCESS.ENV.FB_APP_SECRET',
     GoogleClientID: '208788995431-mqcd39mbl9us33rh1h3eskjlel6a4t64.apps.googleusercontent.com',
     GoogleClientSecret: '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'
 };
