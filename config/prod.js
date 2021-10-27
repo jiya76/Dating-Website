7aadcbc9ee7ba59e9a3c8dae6129e9c8d4259221
Jiya Chauhan <76962214+jiya76@users.noreply.github.com>
2021-10-28 04:31:35 +0530

    mongodb link updated

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,5 @@
 module.exports={
-    MongoDB: 'mongodb+srv://nandini:nansjiya@snuggle.jaud3.mongodb.net/snuggledatabase?retryWrites=true&w=majority',
+    MongoDB: process.env.MONGODB,
     FacebookAppID: '993472138176883',
     FacebookAppSecret: 'e4798f9c72fe78a13346acd8e7ca68db',
     GoogleClientID: '208788995431-mqcd39mbl9us33rh1h3eskjlel6a4t64.apps.googleusercontent.com',
