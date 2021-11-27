d414300be1a37084842404bee0704f05a4defa57
Jiya Chauhan <76962214+jiya76@users.noreply.github.com>
2021-11-27 10:49:04 +0530

    aws and s3 storage

---
+++ helpers/aws.js
@@ -0,0 +1,26 @@
+const aws = require('aws-sdk')
+const multer = require('multer')
+const multer3 = require('multer-s3');
+const keys = require('../config/keys');[]
+
+aws.config.update({
+    accessKeyId: keys.AWSAccessKeyID,
+    secretAccessKey: keys.AWSAccessKeySecret
+});
+
+module.exports = {
+    uploadImage: multer({
+        storage: multers3({
+            s3: new aws.S3({}),
+            bucket: 'snuggle-dating-app',
+            acl: 'public-read',
+            metadata: (req,file,cb) => {
+                cb(null,{fieldName: file.fieldname});
+            },
+            key: (req,file,cb) => {
+
+            }        
+        })
+    })
+}
+
