085c9998109f3cdd354dcadb6a05eee6edc479d8
kishlay123 <78309609+kishlay123@users.noreply.github.com>
2021-12-27 23:22:59 +0530

    Delete prod.js

--- config/prod.js
+++
@@ -1,9 +0,0 @@
-module.exports={
-    MongoDB: process.env.MONGODB,
-    FacebookAppID: process.env.FB_APP,
-    FacebookAppSecret: 'PROCESS.ENV.FB_APP_SECRET',
-    GoogleClientID: '208788995431-mqcd39mbl9us33rh1h3eskjlel6a4t64.apps.googleusercontent.com',
-    GoogleClientSecret: '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',
-    AWSAccessKeyID: process.env.AWS_ACCESS_KEY,
-    AWSAccessKeySecret: process.env.AWS_ACCESS_SECRET
-};
